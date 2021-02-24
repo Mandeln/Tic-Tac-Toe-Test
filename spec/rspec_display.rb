812b7528561cae85b4b38fb717872bce1db7fb72
Mandeln <malistgulvexohmlosur@gmail.com>
2021-02-24 11:42:25 +0100

    add fix changes rspec_display

--- spec/rspec_display.rb
+++ spec/rspec_display.rb
@@ -8,9 +8,9 @@ RSpec.describe Display do
             expect(display.make_board).to eql([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
         end
     end
-    describe '#get_player' do
-        it 'Returns player 2 symbol'
-        expect(display.get_players('X')).to eql ('O') 
+    describe '#get_players' do
+        it 'Returns player 2 symbol' do
+        expect(display.get_players('X')).to eql('O') 
        end
     end
 end
