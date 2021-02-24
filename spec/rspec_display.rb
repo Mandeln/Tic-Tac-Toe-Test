5c5d864e46699fb55689269032c742012217fe7b
Mandeln <malistgulvexohmlosur@gmail.com>
2021-02-24 11:49:38 +0100

    add fix linter errors

--- spec/rspec_display.rb
+++ spec/rspec_display.rb
@@ -1,16 +1,16 @@
-require_relative '../lib/display.rb'
+require_relative '../lib/display'
 
 display = Display.new
 
 RSpec.describe Display do
-    describe '#make_board' do
-        it 'Returns a board with numbers from 1 to 9' do
-            expect(display.make_board).to eql([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-        end
+  describe '#make_board' do
+    it 'Returns a board with numbers from 1 to 9' do
+      expect(display.make_board).to eql([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     end
-    describe '#get_players' do
-        it 'Returns player 2 symbol' do
-        expect(display.get_players('X')).to eql('O') 
-       end
+  end
+  describe '#get_players' do
+    it 'Returns player 2 symbol' do
+      expect(display.get_players('X')).to eql('O')
     end
+  end
 end
--- spec/rspec_game_logic.rb
+++ spec/rspec_game_logic.rb
@@ -1,4 +1,4 @@
-require_relative '../lib/game_logic.rb'
+require_relative '../lib/game_logic'
 
 game_logic = Logic.new('X', 'O')
 test_draw = [%w[X O X], %w[O X X], %w[O X O]]
@@ -8,15 +8,15 @@ test_winingmove = [%w[X O X], %w[O X X], ['O', 'X', 9]]
 test_drawingmove = [%w[X O X], %w[O X X], ['O', 'X', 9]]
 
 RSpec.describe game_logic do
-    describe '#numeric?' do
-        it 'Returns true if the entered number is an integer' do
-          expect(game_logic.numeric?(1)).to eql(true)
-        end
-    
-        it 'Returns false for anything else thats not an Integer' do
-          expect(game_logic.numeric?('Pepeissmol')).to eql(false)
-        end
-      end
+  describe '#numeric?' do
+    it 'Returns true if the entered number is an integer' do
+      expect(game_logic.numeric?(1)).to eql(true)
+    end
+
+    it 'Returns false for anything else thats not an Integer' do
+      expect(game_logic.numeric?('Pepeissmol')).to eql(false)
+    end
+  end
 
   describe '#check_draw' do
     it 'Returns true when the whole board if full' do
@@ -57,28 +57,28 @@ RSpec.describe game_logic do
       expect(game_logic.player_won(test_winingmove, nil, true)).to eql(true)
     end
 
-  describe 'check_if_filled' do
-    it 'Returns true if the position is filled' do
-      expect(game_logic.check_if_filled(test_unfinished, 1)).to eql(true)
-    end
-    it 'Returns false if the position is not filled' do
-      expect(game_logic.check_if_filled(test_unfinished, 5)).to eql(false)
-    end
-  end
-
-  describe 'check_move' do
-    it 'Returns false and the board if the move is not a ending move' do
-      expect(game_logic.check_move(test_unfinished, 5, true)).to eql([false, [%w[X O X], %w[O X X], ['O', 'X', 9]]])
+    describe 'check_if_filled' do
+      it 'Returns true if the position is filled' do
+        expect(game_logic.check_if_filled(test_unfinished, 1)).to eql(true)
+      end
+      it 'Returns false if the position is not filled' do
+        expect(game_logic.check_if_filled(test_unfinished, 5)).to eql(false)
+      end
     end
 
-    it 'Returns true and the board if the move is a ending move' do
-      expect(game_logic.check_move(test_winingmove, 9, true)).to eql([true, [%w[X O X], %w[O X X], %w[O X X]]])
-    end
+    describe 'check_move' do
+      it 'Returns false and the board if the move is not a ending move' do
+        expect(game_logic.check_move(test_unfinished, 5, true)).to eql([false, [%w[X O X], %w[O X X], ['O', 'X', 9]]])
+      end
 
-    it 'Returns true and the board if the move is a drawing move' do
-      expect(game_logic.check_move(test_drawingmove, '9', false)).to eql([true, [%w[X O X], %w[O X X], %w[O X O]]])
+      it 'Returns true and the board if the move is a ending move' do
+        expect(game_logic.check_move(test_winingmove, 9, true)).to eql([true, [%w[X O X], %w[O X X], %w[O X X]]])
+      end
+
+      it 'Returns true and the board if the move is a drawing move' do
+        expect(game_logic.check_move(test_drawingmove, '9', false)).to eql([true, [%w[X O X], %w[O X X], %w[O X O]]])
+      end
     end
-  end 
 
     it 'Returns the same as check_move is the move is not nil' do
       expect(game_logic.player_won(test_winingmove, 9, true)).to eql([true, [%w[X O X], %w[O X X], %w[O X X]]])
